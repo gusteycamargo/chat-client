77132031ecae0b0374e2062d35bd0b50eb789aeb
gustaacamargo <camargo25.gustavo@gmail.com>
2020-05-18 20:59:01 -0300

    Finaliza tela de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,41 @@
 import React, { useState } from 'react' 
 import './styles.css';
 import { IoIosChatboxes } from "react-icons/io";
+import api from '../../services/api';
+import { login } from '../../services/auth';
+import Spinner from 'react-activity/lib/Spinner';
+import 'react-activity/lib/Spinner/Spinner.css';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
-function Login() {
+function Login({ history }) {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const MySwal = withReactContent(Swal);
 
     async function handleSubmit(e) {
         e.preventDefault();
+
+        if (!username || !password) {
+            MySwal.fire('Oops...', 'Parece que você não preencheu todos os campos', 'error');
+        } else {
+            try {
+                setIsLoading(true);
+                const response = await api.post("/sessions", { username, password });
+                console.log(response);
+                
+                login(response.data.token);
+                
+                history.push("/chat");
+            } catch (err) {
+                console.log(err);
+                
+                MySwal.fire('Oops...', 'Nome de usuário ou senha incorreta', 'error');
+            }
+            setIsLoading(false);
+        }
     }
 
     return (
@@ -30,7 +57,10 @@ function Login() {
                         value={password}
                         onChange={e => setPassword(e.target.value)}
                     />
-                    <button className="button" type="submit">Login</button>
+                    <button className="button" type="submit">
+                        Login
+                        <Spinner className="ml-2" color="#7c119c" size={16} speed={0.5} animating={isLoading} />
+                    </button>
                 </form>
             </div>
            
